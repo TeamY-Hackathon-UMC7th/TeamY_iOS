cbe624fce42575a9fadfe4c9404109bb4a368e13
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-11 13:55:15 +0900

    [env] xcode proj 세팅

---
+++ teamY/teamY/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teamY
+//
+//  Created by 김도연 on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
